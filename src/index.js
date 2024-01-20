69bdc77dc80e57ea9a84183a2951e498b301cc0b
Neliswayisipho Maduma <neliswamaduma@gmail.com>
2024-01-20 12:11:10 +0200

    updated src folder

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
-function displayTemperature(response) {
+function refreshWeather(res) {
+  getFourcast(res.data.city);
   let temperatureElement = document.querySelector("#current-temperature");
-  let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.getElementById("description");
+  let humidityElement = document.getElementById("humidity");
+  let windElement = document.getElementById("wind");
+  let iconElement = document.getElementById("icon");
+  let currentDateELement = document.querySelector("#current-date");
+
+  let currentDate = new Date(res.data.time * 1000);
+
+  currentDateELement.innerHTML = formatDate(currentDate);
+
+  let temperature = Math.round(res.data.temperature.current);
+  let humidity = `${res.data.temperature.humidity}%`;
+  let wind = `${res.data.wind.speed}km/h`;
+  let iconWeatherCity = res.data.condition.icon_url;
+
+  cityElement.innerHTML = res.data.city;
   temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = res.data.condition.description;
+  humidityElement.innerHTML = humidity;
+  windElement.innerHTML = wind;
+  iconElement.innerHTML = `<img
+              src="${iconWeatherCity}"
+              alt="The Weather icon"
+            />`;
 }
 
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let city = searchInputElement.value;
-
+function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(displayTemperature);
+  axios.get(apiUrl).then(refreshWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#search-input");
+
+  searchCity(searchInputElement.value);
 }
 
 function formatDate(date) {
@@ -45,9 +70,49 @@ function formatDate(date) {
 }
 
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
+searchForm.addEventListener("submit", handleSubmit);
 
-let currentDateELement = document.querySelector("#current-date");
-let currentDate = new Date();
+searchCity("Durban");
 
-currentDateELement.innerHTML = formatDate(currentDate);
+// -----weather forecast ---
+function formatForcastDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastDay = days[date.getDay()];
+  return forecastDay;
+}
+
+function displayForecast(res) {
+  let forecastElement = document.getElementById("forecast");
+  let forecastHTML = "";
+
+  res.data.daily.forEach((el, index) => {
+    let maxTemperature = Math.round(el.temperature.maximum);
+    let minTemperature = Math.round(el.temperature.minimum);
+    let icone = el.condition.icon_url;
+
+    if (index !== 0) {
+      forecastHTML =
+        forecastHTML +
+        `<li class="forecast-item">
+        <div class="forecast-day">${formatForcastDay(el.time)}</div>
+        <img src="${icone}" alt="The icon of the weather" class="forecast-temperature-icon">
+        <div class="forecast-temperature">
+          <span class="forecast-temperature-max">${maxTemperature} /</span>
+          <span class="forecast-temperature-min">${minTemperature}</span>
+        </div>
+        </li>
+      `;
+    }
+  });
+
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getFourcast(city) {
+  apiKey = "8907b2tf0b29bfcd0b41134b5b1c6ao9";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayForecast);
+}
